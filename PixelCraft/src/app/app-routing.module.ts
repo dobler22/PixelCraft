c4a9acda2f275a844ef5ef1a818a5e9560c33d26
dobler22 <jordirr22@gmail.com>
2023-10-22 23:27:03 -0500

    Agregue el navbar y footer para corregir

--- PixelCraft/src/app/app-routing.module.ts
+++ PixelCraft/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [];
 
